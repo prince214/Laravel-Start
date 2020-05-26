4de6e70e57dcb8874c3d1a153b358605946b6185
prince214 <prince.11604723@lpu.in>
2020-05-27 01:41:13 +0530

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
 
 
 Route::get('{path}',"HomeController@index")->where('path','([A-z\d-\/_.]+)?');
